d6a0be9396354298ede01fa29ada9b21e541ac53
Bruno Alla <alla.brunoo@gmail.com>
2026-09-23 17:15:47 +0100

    DEBUG=False on Fly.io

--- drf_playground/settings.py
+++ drf_playground/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-th0e+80_e5(h*-8!_i8&!e)wzz&b3uc0c*edp-ggcz%$@$b9lu"  # noqa: S105
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DEBUG", "1"))
 
 ALLOWED_HOSTS = ["*"]
 
